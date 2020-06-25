3c71ac54712fa06649e1df31bc78c17b6cfb33c9
MusabShaikhpy <62328097+MusabShaikhpy@users.noreply.github.com>
2020-06-25 06:19:36 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+# CONFIG VARIABLES
+
+URL = 'https://nvd.nist.gov/vuln/data-feeds'
+ANCHOR_TAG = 'a'
+TABLE = "table"
+TR = 'tr'
+TBODY = 'tbody'
+HREF = 'href'
+ID_SCRAPER = {"data-testid":"vuln-json-feed-row-recent-zip"}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
+CVE = 'cve'
+CVE_DATA_META = 'CVE_data_meta'
+ID = 'ID'
+ASSIGNER = 'ASSIGNER'
